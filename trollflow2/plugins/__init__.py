a0a1d10d39efdc06ea1331d42b5a5d5fbc88c368
Panu Lahtinen <pnuu+git@iki.fi>
2023-01-27 10:47:28 +0200

    Remove extra debug logging

--- trollflow2/plugins/__init__.py
+++ trollflow2/plugins/__init__.py
@@ -323,8 +323,6 @@ class FilePublisher:
         self.pub = None
         self.port = port
         self.nameservers = nameservers
-        LOG.debug(f"Publisher settings in __init__: nameservers = {nameservers}, port = {port}")
-        LOG.debug(f"Publisher settings in __init__: self.nameservers = {self.nameservers}, self.port = {self.port}")
         self.__setstate__({'port': port, 'nameservers': nameservers})
 
     def __setstate__(self, kwargs):
@@ -332,8 +330,6 @@ class FilePublisher:
         LOG.debug('Starting publisher')
         self.port = kwargs.get('port', 0)
         self.nameservers = kwargs.get('nameservers', "")
-        LOG.debug(f"kwargs: {kwargs}")
-        LOG.debug(f"Publisher settings in __setstate__: nameservers = {self.nameservers}, port = {self.port}")
         self._pub_starter = create_publisher_from_dict_config(
             {
                 'port': self.port,
