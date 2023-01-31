bb3bad4cc02b100f50adcccb570eec1910952554
Panu Lahtinen <pnuu+git@iki.fi>
2023-01-31 10:48:51 +0200

    Add missing kwarg for passing log config

--- trollflow2/launcher.py
+++ trollflow2/launcher.py
@@ -352,9 +352,9 @@ def get_dask_client(config):
     return client
 
 
-def queue_logged_process(msg, prod_list, produced_files, log_queue):
+def queue_logged_process(msg, prod_list, produced_files, log_queue, log_config=None):
     """Run `process` with a queued log."""
-    setup_queued_logging(log_queue)
+    setup_queued_logging(log_queue, config=log_config)
     with suppress(ValueError):
         signal.signal(signal.SIGUSR1, print_traces)
         LOG.debug("Use SIGUSR1 on pid {} to check the current tracebacks of this subprocess.".format(os.getpid()))
